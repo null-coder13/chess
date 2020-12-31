a80b345f2e4283a8d91df1d4ea9dad39a576e720
Tom <tgadacy@gmail.com>
2020-12-31 01:23:37 -0500

    Created an abstract class for pieces and a game board

---
+++ Chess/src/GameBoard/Board.java
@@ -0,0 +1,9 @@
+package GameBoard;
+
+public class Board {
+    public final char[][] board;
+
+    public Board() {
+        board = new char[7][7];
+    }
+}
---
+++ Chess/src/Pieces/Piece.java
@@ -0,0 +1,8 @@
+package Pieces;
+
+public abstract class Piece {
+    public int team;
+
+    public abstract void move();
+
+}
